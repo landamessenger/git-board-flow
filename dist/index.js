937c270933dc5ab367ad479e29228d7ab07180aa
Efra Espada <efraespada@gmail.com>
2024-12-05 04:52:30 +0100

    gh-action: updated compiled files

--- dist/index.js
+++ dist/index.js
@@ -33001,12 +33001,24 @@ class LinkPullRequestProjectUseCase {
             for (const project of param.projects) {
                 const actionDone = await this.projectRepository.linkContentId(project, param.pullRequest.id, param.tokens.tokenPat);
                 if (actionDone) {
+                    let currentProject = await this.projectRepository.getProjectDetail(project.url, param.tokens.tokenPat);
+                    if (currentProject === undefined) {
+                        result.push(new result_1.Result({
+                            id: this.taskId,
+                            success: false,
+                            executed: true,
+                            steps: [
+                                `Tried to link the pull request to [\`${project.url}\`](${project.url}) but there was a problem.`,
+                            ]
+                        }));
+                        continue;
+                    }
                     result.push(new result_1.Result({
                         id: this.taskId,
                         success: true,
                         executed: true,
                         steps: [
-                            `The pull request was linked to \`${project.url}\`.`,
+                            `The pull request was linked to [**${currentProject?.title}**](${currentProject?.url}).`,
                         ],
                         error: core_1.error,
                     }));
