f37abb59fa8377b235e765b9f18ffcbbc83b8d44
Efra Espada <efraespada@gmail.com>
2025-03-24 21:43:16 +0100

    feature-196-simplify-projecturls-logic: Rename 'project-urls' to 'project-ids' and update related logic; set default for 'commit-prefix-builder' to 'branchName.replace("/", "-")'.

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import * as core from '@actions/core';
+import * as github from "@actions/github";
 import {PullRequestLinkUseCase} from "./data/usecase/pull_request_link_use_case";
 import {IssueLinkUseCase} from "./data/usecase/issue_link_use_case";
 import {ProjectRepository} from "./data/repository/project_repository";
@@ -230,14 +231,15 @@ async function run(): Promise<void> {
     /**
      * Projects Details
      */
-    const projectUrlsInput = core.getInput('project-urls');
-    const projectUrls: string[] = projectUrlsInput
+    const projectIdsInput = core.getInput('project-ids');
+    const projectIds: string[] = projectIdsInput
         .split(',')
-        .map(url => url.trim())
-        .filter(url => url.length > 0);
+        .map(id => id.trim())
+        .filter(id => id.length > 0);
 
     const projects: ProjectDetail[] = []
-    for (const projectUrl of projectUrls) {
+    for (const projectId of projectIds) {
+        const projectUrl = `https://github.com/orgs/${github.context.repo.owner}/projects/${projectId}`
         const detail = await projectRepository.getProjectDetail(projectUrl, tokenPat)
         projects.push(detail)
     }
@@ -533,7 +535,10 @@ async function run(): Promise<void> {
     /**
      * Prefix builder
      */
-    const commitPrefixBuilder = core.getInput('commit-prefix-builder') ?? '';
+    let commitPrefixBuilder = core.getInput('commit-prefix-builder') ?? '';
+    if (commitPrefixBuilder.length === 0) {
+        commitPrefixBuilder = 'branchName.replace("/", "-");';
+    }
 
     /**
      * Issue
