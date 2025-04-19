3070b0089d15af9f5f6be48ae11e6c143238b916
Efra Espada <efraespada@gmail.com>
2025-04-19 18:24:29 +0200

    Remove obsolete TypeScript definition files and compiled binaries from 'bin/' and 'dist/' directories to streamline the project structure and improve maintainability.

--- bin/src/data/model/commit.d.ts
+++
@@ -1,7 +0,0 @@
-export declare class Commit {
-    private inputs;
-    constructor(inputs?: any | undefined);
-    get branchReference(): string;
-    get branch(): string;
-    get commits(): any[];
-}
--- bin/src/data/model/project_detail.d.ts
+++
@@ -1,9 +0,0 @@
-export declare class ProjectDetail {
-    id: string;
-    title: string;
-    type: string;
-    owner: string;
-    url: string;
-    number: number;
-    constructor(data: any);
-}
