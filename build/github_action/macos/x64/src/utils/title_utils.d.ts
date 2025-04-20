3de97e2471e1d64aef912382d910305ee21762f3
Efra Espada <efraespada@gmail.com>
2025-04-20 02:12:21 +0200

    gh-action: updated macos x64 compiled files

---
+++ build/github_action/macos/x64/src/utils/title_utils.d.ts
@@ -0,0 +1,3 @@
+export declare const extractIssueNumberFromBranch: (branchName: string) => number;
+export declare const extractIssueNumberFromPush: (branchName: string) => number;
+export declare const extractVersionFromBranch: (branchName: string) => string | undefined;
