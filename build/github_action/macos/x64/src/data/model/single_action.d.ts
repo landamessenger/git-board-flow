919116d87d6b4427ddfbad135a67b211d535e814
Efra Espada <efraespada@gmail.com>
2025-04-21 00:06:26 +0200

    gh-action: updated macos x64 compiled files

--- build/github_action/macos/x64/src/data/model/single_action.d.ts
+++ build/github_action/macos/x64/src/data/model/single_action.d.ts
@@ -1,13 +1,27 @@
 export declare class SingleAction {
     currentSingleAction: string;
-    currentSingleActionIssue: number;
     actions: string[];
+    /**
+     * Actions that throw an error if the last step failed
+     */
+    actionsThrowError: string[];
     isIssue: boolean;
     isPullRequest: boolean;
     isPush: boolean;
+    /**
+     * Properties
+     */
+    issue: number;
+    version: string;
+    title: string;
+    changelog: string;
     get isDeployedAction(): boolean;
     get isVectorAction(): boolean;
+    get isPublishGithubAction(): boolean;
+    get isCreateReleaseAction(): boolean;
+    get isCreateTagAction(): boolean;
     get enabledSingleAction(): boolean;
     get validSingleAction(): boolean;
-    constructor(currentSingleAction: string, currentSingleActionIssue: string);
+    get throwError(): boolean;
+    constructor(currentSingleAction: string, issue: string, version: string, title: string, changelog: string);
 }
