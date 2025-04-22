b5f7dee2a0656d749382f409f738c47a4df74acc
Efra Espada <efraespada@gmail.com>
2025-04-22 20:47:01 +0000

    gh-action: updated linux x64 compiled files

--- build/cli/linux/x64/src/data/repository/file_repository.d.ts
+++ build/cli/linux/x64/src/data/repository/file_repository.d.ts
@@ -16,8 +16,8 @@ export interface FileTreeNodeWithContent {
 export declare class FileRepository {
     private isMediaOrPdfFile;
     getFileContent: (owner: string, repository: string, path: string, token: string, branch: string) => Promise<string>;
-    getRepositoryContent: (owner: string, repository: string, token: string, branch: string, ignoreFiles: string[], progress: (fileName: string) => void) => Promise<Map<string, string>>;
-    getChunkedRepositoryContent: (owner: string, repository: string, branch: string, chunkSize: number, token: string, ignoreFiles: string[], progress: (fileName: string) => void) => Promise<ChunkedFile[]>;
+    getRepositoryContent: (owner: string, repository: string, token: string, branch: string, ignoreFiles: string[], progress: (fileName: string) => void, ignoredFiles: (fileName: string) => void) => Promise<Map<string, string>>;
+    getChunkedRepositoryContent: (owner: string, repository: string, branch: string, chunkSize: number, token: string, ignoreFiles: string[], progress: (fileName: string) => void, ignoredFiles: (fileName: string) => void) => Promise<ChunkedFile[]>;
     getChunksByLines: (path: string, content: string, chunkSize: number) => ChunkedFile[];
     getChunksByBlocks: (path: string, content: string, chunkSize: number) => ChunkedFile[];
     private shouldIgnoreFile;
@@ -25,7 +25,7 @@ export declare class FileRepository {
     private shouldIgnoreLine;
     private shuffleArray;
     private calculateShasum;
-    getFileTree: (owner: string, repository: string, token: string, branch: string, ignoreFiles: string[], progress: (fileName: string) => void) => Promise<{
+    getFileTree: (owner: string, repository: string, token: string, branch: string, ignoreFiles: string[], progress: (fileName: string) => void, ignoredFiles: (fileName: string) => void) => Promise<{
         withContent: FileTreeNodeWithContent;
         withoutContent: FileTreeNodeWithNoContent;
     }>;
