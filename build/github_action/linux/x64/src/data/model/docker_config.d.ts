883e5b1fbbe0b474454364858f640872d14e3fc7
Efra Espada <efraespada@gmail.com>
2025-10-26 06:51:45 +0000

    gh-action: updated linux x64 compiled files

--- build/github_action/linux/x64/src/data/model/docker_config.d.ts
+++ build/github_action/linux/x64/src/data/model/docker_config.d.ts
@@ -2,8 +2,12 @@ export declare class DockerConfig {
     private containerName;
     private domain;
     private port;
-    constructor(containerName: string, domain: string, port: number);
+    private cacheOs;
+    private cacheArch;
+    constructor(containerName: string, domain: string, port: number, cacheOs: string, cacheArch: string);
     getContainerName(): string;
     getDomain(): string;
     getPort(): number;
+    getCacheOs(): string;
+    getCacheArch(): string;
 }
