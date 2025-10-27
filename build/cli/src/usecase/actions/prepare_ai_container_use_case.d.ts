a0787ba403df33f28fb836cf095b26f054ab31f1
Efra Espada <efraespada@gmail.com>
2025-10-27 18:01:20 +0100

    gh-action: updated compiled files

---
+++ build/cli/src/usecase/actions/prepare_ai_container_use_case.d.ts
@@ -0,0 +1,11 @@
+import { Execution } from '../../data/model/execution';
+import { Result } from '../../data/model/result';
+import { ParamUseCase } from '../base/param_usecase';
+export declare class PrepareAIContainerUseCase implements ParamUseCase<Execution, Result[]> {
+    taskId: string;
+    private dockerRepository;
+    invoke(param: Execution): Promise<Result[]>;
+    private getImageName;
+    private checkImageInRegistry;
+    private buildAndPushImage;
+}
